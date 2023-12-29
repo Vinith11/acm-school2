b950c3ded8cda3a56d885faeac4d1685360f93cb
Vinith <vinith2003@gmail.com>
2023-12-29 14:37:11 +0530

    about us added

--- src/Components/Aboutus.js
+++ src/Components/Aboutus.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Ronaldo from "../assets/Ronaldo.webp"
+import img2 from "../assets/Images/img2.jpg"
 
 function Aboutus() {
   return (
@@ -7,32 +7,20 @@ function Aboutus() {
       <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
         <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
           <div className="md:5/12 lg:w-5/12 ">
-            <img className="rounded-lg " src={Ronaldo} alt="Ronaldo" />
+            <img className="rounded-lg " src={img2} alt="Ronaldo" />
           </div>
           <div className="md:7/12 lg:w-6/12">
             <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
               About Us
             </h2>
             <p className="mt-6 text-gray-600">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem,
-              libero. Facilis ut suscipit, aliquam neque culpa dignissimos natus
-              similique ullam illo officiis velit, libero laboriosam.
+            Welcome to Government Kannada Lower Primary School, a cornerstone of education in our community. Our mission is to empower students with knowledge and foster a love for learning, nurturing well-rounded individuals who contribute positively to society.
             </p>
             <p className="mt-4 text-gray-600">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem,
-              libero. Facilis ut suscipit, aliquam neque culpa dignissimos natus
-              similique ullam illo officiis velit, libero laboriosam.
+            At Government Kannada Lower Primary School, our educational philosophy extends beyond textbooks. We focus on cultivating curiosity, critical thinking, and a passion for lifelong learning. By addressing the intellectual, emotional, and social dimensions of each child, we aim to build a strong foundation for their future endeavors.
             </p>
-            <p className="mt-6 text-gray-600">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem,
-              libero. Facilis ut suscipit, aliquam neque culpa dignissimos natus
-              similique ullam illo officiis velit, libero laboriosam.
-            </p>
-            <p className="mt-6 text-gray-600">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem,
-              libero. Facilis ut suscipit, aliquam neque culpa dignissimos natus
-              similique ullam illo officiis velit, libero laboriosam.
-            </p>
+
+
           </div>
         </div>
       </div>
--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -2,22 +2,18 @@ import React from "react";
 import { galleryImg } from "../assets";
 import GallCard from "./GallCard";
 
-
 const Gallery = () => {
   return (
-    <div  id="gallery" className="py-16">
+    <div id="gallery" className="py-16">
       <div>
         <h2 className="text-2xl font-semibold mb-4 text-center">Event</h2>
       </div>
 
       <div className="flex flex-wrap max-sm:flex-col  gap-4 p-2 justify-center items-center">
         {/* Card 1 */}
-        {
-          galleryImg.map((items)=>(
-            <GallCard key={items.name} {...items}/>
-          ))
-        }
-       
+        {galleryImg.map((items) => (
+          <GallCard key={items.name} {...items} />
+        ))}
       </div>
     </div>
   );
